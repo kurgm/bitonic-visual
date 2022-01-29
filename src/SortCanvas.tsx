9436cb14a9970f808950bca276155dd332fc2558
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2022-01-29 23:49:19 +0900

    Use SVG

--- src/SortCanvas.tsx
+++ src/SortCanvas.tsx
@@ -7,15 +7,15 @@ const getTransforms = (nElem: number, network: BitonicNetwork): React.CSSPropert
   const result = new Array<React.CSSProperties>(nElem).fill({});
   for (const [smallIdx, largeIdx] of network.pairs) {
     if (network.orderType === "flip") {
-      const originOffset = (largeIdx - smallIdx + 1) / 2;
+      const origin = (largeIdx + smallIdx + 1) / 2;
       result[smallIdx] = {
         transform: "rotateY(180deg)",
-        transformOrigin: `${originOffset * 100}% 50%`
+        transformOrigin: `${origin}px 50%`
       };
     } else {
       const shiftAmount = largeIdx - smallIdx;
       result[smallIdx] = {
-        transform: `translateX(${shiftAmount * 100}%)`,
+        transform: `translateX(${shiftAmount}px)`,
       };
     }
   }
@@ -33,36 +33,34 @@ export interface SortCanvasProps {
 
 const SortCanvas: React.FC<SortCanvasProps> = (props) => {
   const { width, height, array, phase, network, onTransitionEnd } = props;
-  const n = array.length;
-  const binWidth = width / n;
+  const nElem = array.length;
   const maxValue = Math.max(...array);
   const csses = React.useMemo(() => (
-    network ? getTransforms(n, network) : null
-  ), [n, network]);
+    network ? getTransforms(nElem, network) : null
+  ), [nElem, network]);
   return (
-    <div className="canvas" style={{
-      height: `${height}px`,
-      width: `${width}px`,
-    }}>
-      {array.map((n, i) => {
-        const { transform = undefined, ...css } = csses ? csses[i] : {};
-        return (
-          <div
-            key={i}
-            className="bar"
-            style={{
-              bottom: "0px",
-              height: `${height / maxValue * n}px`,
-              left: `${binWidth * i}px`,
-              width: `${binWidth}px`,
-              ...(phase === Phase.animationIn) && { transform },
-              ...css,
-            }}
-            {...(i === 0) && { onTransitionEnd }}
-          />
-        );
-      })}
-    </div>
+    <svg className="canvas" height={height} width={width}>
+      <g transform={`translate(0,${height}) scale(${width / nElem},${-height / maxValue})`}>
+        {array.map((value, i) => {
+          const { transform = undefined, ...css } = csses ? csses[i] : {};
+          return (
+            <rect
+              key={i}
+              className="bar"
+              x={i}
+              y={0}
+              width={1}
+              height={value}
+              style={{
+                ...(phase === Phase.animationIn) && { transform },
+                ...css,
+              }}
+              {...(i === 0) && { onTransitionEnd }}
+            />
+          );
+        })}
+      </g>
+    </svg>
   )
 };
 
