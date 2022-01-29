8f98eeed8380f6a16b0c912413d48981763bd25e
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2022-01-29 15:00:05 +0900

    Convert state to hooks

--- src/BitonicSort.tsx
+++ src/BitonicSort.tsx
@@ -29,103 +29,101 @@ export interface IBitonicSortProps {
   width: number;
   height: number;
 }
-export interface IBitonicSortState {
+interface IBitonicSortState {
   array: number[];
   completed: boolean;
-  nonstop: boolean;
   phase: Phase;
   stage: [number, number];
   variant: SortVariant;
 }
 
-export default class BitonicSort extends React.Component<IBitonicSortProps, IBitonicSortState> {
-  public state: IBitonicSortState = {
+const BitonicSort: React.FC<IBitonicSortProps> = (props) => {
+  const [state, setState_] = React.useState<IBitonicSortState>({
     array: randomArray(32),
     completed: false,
-    nonstop: false,
     phase: Phase.waiting,
     stage: [1, 0],
     variant: SortVariant.monotonic,
+  });
+  const setState = (newState: Partial<IBitonicSortState>) => {
+    setState_((state) => ({ ...state, ...newState }));
   };
-  public render(): JSX.Element {
-    const network = bitonicSortNetwork(this.state.variant, this.state.array.length, this.state.stage);
-    return (
-      <div>
-        <SortCanvas
-          width={this.props.width}
-          height={this.props.height}
-          array={this.state.array}
-          phase={this.state.phase}
-          network={network}
-          onTransitionEnd={this.handleTransitionEnd}
-        />
-        <div className="controller">
-          <StepController
-            canStep={this.state.phase === Phase.waiting}
-            onStep={this.handleStep}
-            nonstop={this.state.nonstop}
-            onNonstopChange={this.handleNonstop}
-          />
-          <OptionController
-            canReset={this.state.phase === Phase.waiting}
-            onReset={this.handleReset}
-          />
-        </div>
-      </div>
-    );
-  }
 
-  private handleStep = () => {
-    if (this.state.phase !== Phase.waiting || this.state.completed) {
+  const [nonstop, setNonstop] = React.useState(false);
+
+  const handleStep = React.useCallback(() => {
+    if (state.phase !== Phase.waiting || state.completed) {
       return;
     }
-    this.setState({
+    setState({
       phase: Phase.animationIn,
     });
-  }
-  private handleTransitionEnd = () => {
-    if (this.state.phase === Phase.animationIn) {
-      this.handleAnimationInEnd();
-    } else if (this.state.phase === Phase.animationOut) {
-      this.handleAnimationOutEnd();
-    }
-  }
-  private handleAnimationInEnd = () => {
-    const network = bitonicSortNetwork(this.state.variant, this.state.array.length, this.state.stage);
-    const newArray = bitonicSortStep(this.state.array, network);
-    this.setState({
+  }, [state]);
+  const handleAnimationInEnd = React.useCallback(() => {
+    const network = bitonicSortNetwork(state.variant, state.array.length, state.stage);
+    const newArray = bitonicSortStep(state.array, network);
+    setState({
       array: newArray,
       phase: Phase.animationOut,
     });
-  }
-  private handleAnimationOutEnd = () => {
-    const newStage: [number, number] = [this.state.stage[0], this.state.stage[1] - 1];
+  }, [state]);
+  const handleAnimationOutEnd = React.useCallback(() => {
+    const newStage: [number, number] = [state.stage[0], state.stage[1] - 1];
     if (newStage[1] === -1) {
       newStage[0]++;
       newStage[1] = newStage[0] - 1;
     }
-    const completed = this.state.array.length < (1 << newStage[0]);
-    this.setState({
+    const completed = state.array.length < (1 << newStage[0]);
+    setState({
       completed,
-      phase: this.state.nonstop && !completed ? Phase.animationIn : Phase.waiting,
+      phase: nonstop && !completed ? Phase.animationIn : Phase.waiting,
       stage: newStage,
     });
-  }
-  private handleReset = (opt: ResetOption) => {
-    if (this.state.phase !== Phase.waiting) {
+  }, [state, nonstop]);
+  const handleTransitionEnd = React.useCallback(() => {
+    if (state.phase === Phase.animationIn) {
+      handleAnimationInEnd();
+    } else if (state.phase === Phase.animationOut) {
+      handleAnimationOutEnd();
+    }
+  }, [state, handleAnimationInEnd, handleAnimationOutEnd]);
+  const handleReset = React.useCallback((opt: ResetOption) => {
+    if (state.phase !== Phase.waiting) {
       return;
     }
-    this.setState({
+    setState({
       array: randomArray(opt.nElem),
       completed: false,
       phase: Phase.waiting,
       stage: [1, 0],
       variant: opt.sortVariant,
     });
-  }
-  private handleNonstop = (nonstop: boolean) => {
-    this.setState({
-      nonstop,
-    });
-  }
-}
+  }, [state]);
+  const network = bitonicSortNetwork(state.variant, state.array.length, state.stage);
+  return (
+    <div>
+      <SortCanvas
+        width={props.width}
+        height={props.height}
+        array={state.array}
+        phase={state.phase}
+        network={network}
+        onTransitionEnd={handleTransitionEnd}
+      />
+      <div className="controller">
+        <StepController
+          canStep={state.phase === Phase.waiting}
+          onStep={handleStep}
+          nonstop={nonstop}
+          onNonstopChange={setNonstop}
+        />
+        <OptionController
+          canReset={state.phase === Phase.waiting}
+          onReset={handleReset}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BitonicSort;
