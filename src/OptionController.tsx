9a0b2e74a7123f521d07965ed1d67997b330e990
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2022-01-30 14:22:41 +0900

    Extract useSelect

--- src/OptionController.tsx
+++ src/OptionController.tsx
@@ -19,20 +19,46 @@ export interface OptionControllerProps {
   onReset: (opt: ResetOption) => void;
 }
 
+type OptionDesc<T extends string | number> =
+  | T
+  | React.OptionHTMLAttributes<HTMLOptionElement> & { value: T; };
+
+const useSelect = <T extends string | number>(
+  initialValue: T | (() => T),
+  options: readonly OptionDesc<T>[],
+  props?: React.SelectHTMLAttributes<HTMLSelectElement>,
+): [value: T, element: JSX.Element] => {
+
+  const [value, setValue] = React.useState<T>(initialValue);
+  const onChange = React.useCallback((evt: React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue = evt.currentTarget.value;
+    setValue((oldValue) => (typeof oldValue === "number" ? Number(newValue) : newValue) as T);
+  }, []);
+
+  const element = (
+    <select {...props} value={value} onChange={onChange}>
+      {options.map((option, i) => {
+        if (typeof option !== "object") {
+          option = {
+            children: option,
+            value: option,
+          };
+        }
+        return (
+          <option key={i} {...option} />
+        );
+      })}
+    </select>
+  );
+  return [value, element];
+};
+
 const OptionController: React.FC<OptionControllerProps> = (props) => {
   const { onReset } = props;
 
-  const [nElem, setNElem] = React.useState(defaultOption.nElem);
-  const [sortVariant, setSortVariant] = React.useState(defaultOption.sortVariant);
+  const [nElem, nElemSelect] = useSelect(defaultOption.nElem, nElem_options);
+  const [sortVariant, sortVariantSelect] = useSelect(defaultOption.sortVariant, sortVariants);
 
-  const handleNElemChange = React.useCallback((evt: React.ChangeEvent<HTMLSelectElement>) => {
-    const newNElem = parseInt(evt.currentTarget.value, 10);
-    setNElem(newNElem);
-  }, []);
-  const handleVariantChange = React.useCallback((evt: React.ChangeEvent<HTMLSelectElement>) => {
-    const newMode = evt.currentTarget.value as SortVariant;
-    setSortVariant(newMode);
-  }, []);
   const handleReset = React.useCallback(() => {
     onReset({
       nElem,
@@ -43,20 +69,10 @@ const OptionController: React.FC<OptionControllerProps> = (props) => {
   return (
     <div>
       <label>
-        N =
-        <select onChange={handleNElemChange} value={nElem}>
-          {nElem_options.map((n) => (
-            <option key={n} value={n}>{n}</option>
-          ))}
-        </select>
+        N = {nElemSelect}
       </label>
       <label>
-        Mode:
-        <select onChange={handleVariantChange} value={sortVariant}>
-          {sortVariants.map((sortVariant) => (
-            <option key={sortVariant} value={sortVariant}>{sortVariant}</option>
-          ))}
-        </select>
+        Mode: {sortVariantSelect}
       </label>
       <button onClick={handleReset} disabled={!props.canReset}>reset</button>
     </div>
