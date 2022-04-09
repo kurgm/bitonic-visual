5ef310f6cb4eab557d8b983d18f53bb62135aa30
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2022-04-09 11:04:56 +0900

    Use ReactDOM.createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 
 import BitonicSort from "./BitonicSort";
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
+  ReactDOM.createRoot(document.getElementById("app")!).render(
     <BitonicSort
       width={384}
       height={288}
     />,
-    document.getElementById("app"),
   );
 });
